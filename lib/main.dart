b57a3e28ab104bde6eadd7877c0936fccf0960e3
neenu-as <neenuajithkumar@gmail.com>
2023-10-28 15:52:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+void main()
+{
+
+  runApp(MaterialApp(home: Profile2(),));
+
+
+}
+class Profile2 extends StatefulWidget {
+  const profile2({super.key});
+
+  @override
+  State<profile2> createState() => _profile2State();
+}
+
+class _profile2State extends State<profile2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
